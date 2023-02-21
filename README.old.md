047dfdb8e0a6f9716326c9d2261d4417c6149a6e
mv23c <100789090+mv23c@users.noreply.github.com>
2023-02-21 16:29:07 -0300

    Adição de state e eventos sintéticos

--- README.old.md
+++ README.old.md
@@ -1,2 +1,20 @@
-# projeto_secao2
-Projeto desenvolvido na seção 2 (React, o básico) do curso de React.Js e Next.Js do instrutor Luiz Otávio Miranda
+# Notas sobre o projeto
+
+## Componente de classe
+Para criar um componente de classe, fazemos:
+
+```jsx
+class App extends Component {
+    render() {
+        return <h1>Olá mundo</h1>
+    }
+}
+```
+Ou seja, criamos uma classe cujo nome inicia com letra maiúscula e que estende Component (que deve ser importado). Essa classe contém um método render() que vai retornar um jsx.
+O método render() é responsável por renderizar o conteúdo jsx que deve ser exibido na tela, com base no estado atual do componente e nas props recebidas. É um método obrigátorio em componentes de classe. 
+
+
+## State
+
+Todo componente pode ter estado e este é basicamente os dados desse componente.
+O estado (state) em react é um objeto que contém as informações (dados) internas de um componente e pode ser alterado ao longo do tempo, afetando renderizzação do componente e de seus filhos. 
--- src/App.js
+++ src/App.js
@@ -3,47 +3,45 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  state = {
+    name: 'Mário Vinícius',
+    counter: 0
+  };
+  
+  handlePClick = () => {
+    this.setState({ name: 'Jacinta Santos' });
+
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
   render() {
+    const { name, counter } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Olá mundo!
+          <p onClick={this.handlePClick}>
+            {name} {counter}
           </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Este é o link!
           </a>
         </header>
       </div>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Olá mundo!
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
